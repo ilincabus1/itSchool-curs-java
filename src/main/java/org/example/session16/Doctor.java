ab31b8822d83ae9237ceec61af0a01b84ad1f0a8
Ilinca Busuioc <busuiocilinca@gmail.com>
2025-07-23 20:46:47 +0300

    demo OOP

---
+++ src/main/java/org/example/session16/Doctor.java
@@ -0,0 +1,13 @@
+package main.java.org.example.session16;
+
+public class Doctor extends Person{
+
+    public Doctor(String name, String id) {
+        super(name, id);
+    }
+
+    @Override
+    public void performDuties() {
+        System.out.println(getName() + " performs doctor duties");
+    }
+}
---
+++ src/main/java/org/example/session16/Nurse.java
@@ -0,0 +1,13 @@
+package main.java.org.example.session16;
+
+public class Nurse extends Person {
+
+    public Nurse(String name, String id) {
+        super(name, id);
+    }
+
+    @Override
+    public void performDuties() {
+        System.out.println(getName() + " perform nurse duties");
+    }
+}
